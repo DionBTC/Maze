a172e225dff347b195addc5ad6746efe459d69a6
Dion Mascarenhas <dionmascarenhas3030@gmail.com>
2024-10-12 13:21:19 +0100

    Add files via upload

---
+++ PhysicsController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsController : MonoBehaviour
+{
+	//PSEUDO CODE: Declare private variable of type RigidBody, public float speed;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		// Initialize RigidBody variable by getting from current gameobject;
+	}
+
+    // Fixed Update is called once per frame - use fixed Update for physics
+    void FixedUpdate()
+    {
+
+		// Check for "a", "d", "w", and "d" key input, for each, add force vector3s going left, right, forward, and back
+
+		{
+
+		}
+		
+		{
+
+		}
+
+		{
+
+		}
+		
+		{
+			
+			//Stretch Task - add multiple to increase force
+		}
+	}
+
+	//Stretch Tasks
+	// Unity Rollaball (Combines forces in Vector https://learn.unity.com/project/roll-a-ball-tutorial)
+	// Intermediate controllers https://medium.com/ironequal/unity-character-controller-vs-rigidbody-a1e243591483
+
